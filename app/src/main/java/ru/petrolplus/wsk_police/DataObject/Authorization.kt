13eb405219d6207d68a929567cee04d648159ecf
Vladimir Kaushkin <vladimir.kaushkin@fleetcor.ru>
2019-06-07 00:21:50 +0300

    добавление объектов для авторизации

---
+++ app/src/main/java/ru/petrolplus/wsk_police/DataObject/Authorization.kt
@@ -0,0 +1,4 @@
+package ru.petrolplus.wsk_police.DataObject
+
+
+data class Authorization(var data: User ,var success: Boolean, var error: String)
---
+++ app/src/main/java/ru/petrolplus/wsk_police/DataObject/User.kt
@@ -0,0 +1,4 @@
+package ru.petrolplus.wsk_police.DataObject
+
+
+data class User(var id: String, var login: String, var name: String, var token: String)
