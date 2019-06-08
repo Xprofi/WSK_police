cde5bd09a8a7ff32188cdf9cab3210f0e8f03274
Vladimir Kaushkin <vladimir.kaushkin@fleetcor.ru>
2019-06-08 15:04:20 +0300

    add http client

---
+++ app/src/main/java/ru/petrolplus/wsk_police/Utils/NetworkClient.kt
@@ -0,0 +1,31 @@
+package ru.petrolplus.wsk_police.Utils
+
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+class NetworkClient(){
+
+
+    private val ROOT_URL_API: String  = "mad2019.hakta.pro"
+
+
+    private fun httpClient(): Retrofit {
+
+        val httpClient = OkHttpClient.Builder()
+            .connectTimeout(100, TimeUnit.SECONDS)
+            .readTimeout(100, TimeUnit.SECONDS)
+            .callTimeout(100, TimeUnit.SECONDS)
+            .writeTimeout(100, TimeUnit.SECONDS)
+
+        return  Retrofit.Builder()
+            .baseUrl(ROOT_URL_API)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(httpClient.build())
+            .build()
+    }
+
+
+
+}
