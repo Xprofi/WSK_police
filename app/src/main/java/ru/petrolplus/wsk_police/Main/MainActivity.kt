2a9e7803e4bb10d5bb65726cad0b9168234fe9a7
Vladimir Kaushkin <vladimir.kaushkin@fleetcor.ru>
2019-06-12 04:10:04 +0300

    refactor

---
+++ app/src/main/java/ru/petrolplus/wsk_police/Main/MainActivity.kt
@@ -0,0 +1,13 @@
+package ru.petrolplus.wsk_police.Main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.petrolplus.wsk_police.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
