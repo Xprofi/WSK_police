8aa4c8e3de4aa396237e0504309228823113dd9b
Vladimir Kaushkin <vladimir.kaushkin@fleetcor.ru>
2019-06-16 03:45:50 +0300

    action bar 0.1

--- app/src/main/java/ru/petrolplus/wsk_police/Main/MainActivity.kt
+++ app/src/main/java/ru/petrolplus/wsk_police/Main/MainActivity.kt
@@ -1,13 +1,22 @@
 package ru.petrolplus.wsk_police.Main
 
-import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
+import android.view.ViewGroup
+import androidx.appcompat.app.ActionBar
+import androidx.constraintlayout.widget.ConstraintLayout
+import ru.petrolplus.wsk_police.BaseActivityFun
 import ru.petrolplus.wsk_police.R
 
-class MainActivity : AppCompatActivity() {
+
+
+
+class MainActivity : BaseActivityFun() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        actionBar()
+
     }
 }
